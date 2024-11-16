ad3136d5b764755809818c6180033778f9111f54
psemmel <psemmel@student.ethz.ch>
2024-11-16 16:56:08 +0100

    manual revert

--- workspace/Core/Src/main.c
+++ workspace/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "helper_functions.h"
+#include "AS7265x.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,6 +54,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
+HAL_StatusTypeDef A7265x_Read(I2C_HandleTypeDef *hi2c, uint8_t reg, uint8_t *data, uint16_t size);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -94,12 +96,15 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  printf("Test\r\n");
+  uint8_t data = 0;
+  A7265x_Read(&hi2c1, A7265X_HW_VERSION_H, &data, 1);
+  printf("Test data: 0b%x\r\n", data);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -258,7 +263,19 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+HAL_StatusTypeDef A7265x_Read(I2C_HandleTypeDef *hi2c, uint8_t reg, uint8_t *data, uint16_t size)
+{
+    HAL_StatusTypeDef status;
 
+    status = HAL_I2C_Master_Transmit(hi2c, (uint16_t)(A7265x_I2C_MAXSTER_WRITE_ADDR), &reg, 1, 1000);
+    if (status != HAL_OK)
+    {
+        return status;  // If there's an issue with writing the register address, return the error
+    }
+
+    // Then, read the data from the module
+    return HAL_I2C_Master_Receive(hi2c, (uint16_t)(A7265x_I2C_MAXSTER_READ_ADDR), data, size, 1000);
+}
 /* USER CODE END 4 */
 
 /**
@@ -270,8 +287,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
